4baff5c02c1a60ff74475056a58f01462ad9570b
Swayam <chavan.swayam2006@gmail.com>
2026-07-20 23:10:29 +0530

    sixth commit

--- api/index.js
+++ api/index.js
@@ -1,5 +1,2 @@
-// Vercel serverless function entry point
-import serverless from 'serverless-http';
-import app from '../server/app.js'; // Adjust path relative to this file
-
-export const handler = serverless(app);
+import app from '../server/app.js';
+export default app;
